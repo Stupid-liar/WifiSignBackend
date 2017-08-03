dae9a0cad79174900f9b7f6c1dcaaf1b81d527fc
Dongdong <1910794131@qq.com>
2017-08-03 20:45:21 +0800

    删除py文件

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-main_page = Blueprint('main_page',
-                      __name__,
-                      url_prefix='/main_page',
-                      template_folder='templates',
-                      static_folder='assets')
-
-from . import views
