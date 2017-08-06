b25c2e07778195b1460f39a1a0c0072658f90bd0
Dongdong <1910794131@qq.com>
2017-08-06 18:30:24 +0800

    未解决跳转冲突

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,91 +1,19 @@
 
-jQuery(document).ready(function() {
 
-    $('.page-container .register form').submit(function(){
-		
-        var username = $(this).find('.username').val();
-        var password = $(this).find('.password').val();
-		var nickname = $(this).find('.nickname').val();
-		var email = $(this).find('.email').val();
-		var classId = $(this).find('.class_id').val();
-        if(username == '') {
-            $(this).find('.error').fadeOut('fast', function(){
-                $(this).css('top', '27px');
-            });
-            $(this).find('.error').fadeIn('fast', function(){
-                $(this).parent().find('.username').focus();
-            });
-            return false;
-        }
-        
-		if(nickname == '') {
-            $(this).find('.error').fadeOut('fast', function(){
-                $(this).css('top', '96px');
-            });
-            $(this).find('.error').fadeIn('fast', function(){
-                $(this).parent().find('.nickname').focus();
-            });
-            return false;
-        }
-		if(email == '') {
-            $(this).find('.error').fadeOut('fast', function(){
-                $(this).css('top', '165px');
-            });
-            $(this).find('.error').fadeIn('fast', function(){
-                $(this).parent().find('.email').focus();
-            });
-            return false;
-        }
-		if(password == '') {
-            $(this).find('.error').fadeOut('fast', function(){
-                $(this).css('top', '234px');
-            });
-            $(this).find('.error').fadeIn('fast', function(){
-                $(this).parent().find('.password').focus();
-            });
-            return false;
-        }
-		if(classId == '') {
-            $(this).find('.error').fadeOut('fast', function(){
-                $(this).css('top', '304px');
-            });
-            $(this).find('.error').fadeIn('fast', function(){
-                $(this).parent().find('.class_id').focus();
-            });
-            return false;
-        }
-    });
-	
-	
-	
-	$('.page-container .login form').submit(function(){
-		
-		var password = $(this).find('.password').val();
-		var email = $(this).find('.email').val();
-       
-		if(email == '') {
-            $(this).find('.error').fadeOut('fast', function(){
-                $(this).css('top', '27px');
-            });
-            $(this).find('.error').fadeIn('fast', function(){
-                $(this).parent().find('.email').focus();
-            });
-            return false;
-        }
-		if(password == '') {
-            $(this).find('.error').fadeOut('fast', function(){
-                $(this).css('top', '96px');
-            });
-            $(this).find('.error').fadeIn('fast', function(){
-                $(this).parent().find('.password').focus();
-            });
-            return false;
-        }
-    });
-	
+	(function checkSession(){
+		alert(21456);
+		$.ajax({
+			url: 'http://119.29.184.156:9000/admin/list', //在这里提填写你的地址
+			async: false,
+			dataType: 'jsonp',
+			data:{'student_name':"万"},
+			type:'get',
+			jsonp:"callback_info", 
+			crossDomain: true,
+			jsonpCallback:"callback_info",
+			success: function (json_str) {
+				location.href="http://www.baudu.com";
+			}
+		});
+	})();
 
-    $('.page-container form .username, .page-container form .password').keyup(function(){
-        $(this).parent().find('.error').fadeOut('fast');
-    });
-
-});
